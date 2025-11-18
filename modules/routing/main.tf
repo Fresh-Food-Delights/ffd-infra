f49834df0401124e2168726378e6b2c9ce5036a6
Tyler Berrington, Gentleman <tylerberrington@student.purdueglobal.edu>
2025-11-18 13:27:51 -0600

    terraform validate fixes

--- modules/routing/main.tf
+++ modules/routing/main.tf
@@ -48,37 +48,37 @@ resource "aws_route" "web_nat" {
   nat_gateway_id         = each.value
 }
 
-# Route from Web Tier to S3 (via VPCe)
-resource "aws_route" "web_to_s3" {
-  count                  = var.vpce_s3_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_web.id
-  destination_cidr_block = "s3"
-  vpc_endpoint_id        = var.vpce_s3_id
-}
+## Route from Web Tier to S3 (via VPCe)
+#resource "aws_route" "web_to_s3" {
+#  count                  = var.vpce_s3_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_web.id
+#  destination_cidr_block = "s3"
+#  vpc_endpoint_id        = var.vpce_s3_id
+#}
 
 resource "aws_route" "web_to_app" {
   route_table_id         = aws_route_table.private_web.id
   destination_cidr_block = var.app_cidr_block
 }
 
-resource "aws_route" "web_to_ssm" {
-  count                  = var.vpce_ssm_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_web.id
-  destination_cidr_block = "ssm"
-  vpc_endpoint_id        = var.vpce_ssm_id
-}
-resource "aws_route" "web_to_ssmmessages" {
-  count                  = var.vpce_ssmmessages_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_web.id
-  destination_cidr_block = "ssmmessages"
-  vpc_endpoint_id        = var.vpce_ssmmessages_id
-}
-resource "aws_route" "web_to_ec2messages" {
-  count                  = var.vpce_ec2messages_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_web.id
-  destination_cidr_block = "ec2messages"
-  vpc_endpoint_id        = var.vpce_ec2messages_id
-}
+#resource "aws_route" "web_to_ssm" {
+#  count                  = var.vpce_ssm_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_web.id
+#  destination_cidr_block = "ssm"
+#  vpc_endpoint_id        = var.vpce_ssm_id
+#}
+#resource "aws_route" "web_to_ssmmessages" {
+#  count                  = var.vpce_ssmmessages_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_web.id
+#  destination_cidr_block = "ssmmessages"
+#  vpc_endpoint_id        = var.vpce_ssmmessages_id
+#}
+#resource "aws_route" "web_to_ec2messages" {
+#  count                  = var.vpce_ec2messages_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_web.id
+#  destination_cidr_block = "ec2messages"
+#  vpc_endpoint_id        = var.vpce_ec2messages_id
+#}
 
 # Routes for Private App (to Web + DB + VPCe)
 resource "aws_route" "app_to_web" {
@@ -91,40 +91,40 @@ resource "aws_route" "app_to_db" {
   destination_cidr_block = var.db_cidr_block
 }
 
-resource "aws_route" "app_to_s3" {
-  count                  = var.vpce_s3_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_app.id
-  destination_cidr_block = "s3"
-  vpc_endpoint_id        = var.vpce_s3_id
-}
+#resource "aws_route" "app_to_s3" {
+#  count                  = var.vpce_s3_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_app.id
+#  destination_cidr_block = "s3"
+#  vpc_endpoint_id        = var.vpce_s3_id
+#}
 
-resource "aws_route" "app_to_dynamodb" {
-  count                  = var.vpce_dynamodb_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_app.id
-  destination_cidr_block = "dynamodb"
-  vpc_endpoint_id        = var.vpce_dynamodb_id
-}
+#resource "aws_route" "app_to_dynamodb" {
+#  count                  = var.vpce_dynamodb_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_app.id
+#  destination_cidr_block = "dynamodb"
+#  vpc_endpoint_id        = var.vpce_dynamodb_id
+#}
 
-resource "aws_route" "app_to_ssm" {
-  count                  = var.vpce_ssm_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_app.id
-  destination_cidr_block = "ssm"
-  vpc_endpoint_id        = var.vpce_ssm_id
-}
+#resource "aws_route" "app_to_ssm" {
+#  count                  = var.vpce_ssm_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_app.id
+#  destination_cidr_block = "ssm"
+#  vpc_endpoint_id        = var.vpce_ssm_id
+#}
 
-resource "aws_route" "app_to_secrets" {
-  count                  = var.vpce_secretsmanager_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_app.id
-  destination_cidr_block = "secretsmanager"
-  vpc_endpoint_id        = var.vpce_secretsmanager_id
-}
+#resource "aws_route" "app_to_secrets" {
+#  count                  = var.vpce_secretsmanager_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_app.id
+#  destination_cidr_block = "secretsmanager"
+#  vpc_endpoint_id        = var.vpce_secretsmanager_id
+#}
 
-resource "aws_route" "app_to_kms" {
-  count                  = var.vpce_kms_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_app.id
-  destination_cidr_block = "kms"
-  vpc_endpoint_id        = var.vpce_kms_id
-}
+#resource "aws_route" "app_to_kms" {
+#  count                  = var.vpce_kms_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_app.id
+#  destination_cidr_block = "kms"
+#  vpc_endpoint_id        = var.vpce_kms_id
+#}
 
 # Routes for Private DB (to App + VPCe)
 resource "aws_route" "db_to_app" {
@@ -132,12 +132,12 @@ resource "aws_route" "db_to_app" {
   destination_cidr_block = var.app_cidr_block
 }
 
-resource "aws_route" "db_to_s3" {
-  count                  = var.vpce_s3_id != null ? 1 : 0
-  route_table_id         = aws_route_table.private_db.id
-  destination_cidr_block = "s3"
-  vpc_endpoint_id        = var.vpce_s3_id
-}
+#resource "aws_route" "db_to_s3" {
+#  count                  = var.vpce_s3_id != null ? 1 : 0
+#  route_table_id         = aws_route_table.private_db.id
+#  destination_cidr_block = "s3"
+#  vpc_endpoint_id        = var.vpce_s3_id
+#}
 
 # Route Table Associations (Public)
 resource "aws_route_table_association" "public" {
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,7 +5,7 @@ output "vpc_id" {
   value       = aws_vpc.this.id
 }
 
-output "igw_id" {
+output "internet_gateway_id" {
+  value = aws_internet_gateway.this.id
   description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.this.id
 }
