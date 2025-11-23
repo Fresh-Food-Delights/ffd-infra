6fc452860969ea477379761771cf5f9c84bfe111
Jessica <jessicaclinton@student.purdueglobal.edu>
2025-11-23 17:58:25 +0100

    feature/dynamodb/main mods S3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,10 +1,14 @@
 # modules/s3/main.tf
 
+provider "aws" {
+  region = "us-east-1"  # or your chosen region
+}
+
 resource "aws_s3_bucket" "this" {
- bucket = var.bucket_name
+  bucket = var.bucket_name
 
   tags = {
-   Name        = var.bucket_name
-   Environment = var.environment
+    Name        = var.bucket_name
+    Environment = var.environment
   }
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -14,3 +14,26 @@ variable "bucket_name" {
   description = "The unique name for the S3 bucket."
   type        = string
 }
+variable "policy" {
+  description = "S3 bucket policy"
+  type        = string
+  default     = ""
+}
+
+variable "ownership_policy" {
+  description = "Ownership policy for the S3 bucket"
+  type        = string
+  default     = ""
+}
+
+variable "public_block_policy" {
+  description = "Public block policy for the S3 bucket"
+  type        = string
+  default     = ""
+}
+
+variable "sse_policy" {
+  description = "SSE policy for the S3 bucket"
+  type        = string
+  default     = ""
+}
