36b858f4b476bdd26d131c04f2278ef3bbf4da77
abdulkazeembisiriyu-purdueglobal <abdulkazeembisir@student.purdueglobal.edu>
2025-11-23 23:26:27 -0500

    feat: add IT admin and ops IAM user groups (#51)

---
+++ modules/iam/main.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_group" "it_admin_group" {
+  name = "ffd-aws-it-admin-${var.environment}-7714022395766"
+}
+
+resource "aws_iam_group" "it_ops_group" {
+  name = "ffd-aws-it-ops-${var.environment}-7714022395766"
+}
+
+# Admin group: full admin access
+resource "aws_iam_group_policy_attachment" "it_admin_admin_access" {
+  group      = aws_iam_group.it_admin_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Ops group: read-only access
+resource "aws_iam_group_policy_attachment" "it_ops_readonly_access" {
+  group      = aws_iam_group.it_ops_group.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
