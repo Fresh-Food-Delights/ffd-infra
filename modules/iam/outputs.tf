c1dcd91743b94c78c6377d7ab215be0a43ee0c95
Jessica <jessicaclinton@student.purdueglobal.edu>
2025-11-29 14:01:29 +0100

    Final Fix: Corrected invalid Windows Base AMI ID for us-east-1 and finalized S3 bucket name

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -2,20 +2,20 @@
 
 output "admin_group_name" {
   description = "Name of the IT admin IAM group"
-  value       = aws_iam_group.it_admin.name
+  value       = aws_iam_group.it_admin_group.name
 }
 
 output "admin_group_id" {
   description = "ID of the IT admin IAM group"
-  value       = aws_iam_group.it_admin.id
+  value       = aws_iam_group.it_admin_group.id
 }
 
 output "ops_group_name" {
   description = "Name of the IT ops IAM group"
-  value       = aws_iam_group.it_ops.name
+  value       = aws_iam_group.it_ops_group.name
 }
 
 output "ops_group_id" {
   description = "ID of the IT ops IAM group"
-  value       = aws_iam_group.it_ops.id
+  value       = aws_iam_group.it_ops_group.id
 }
